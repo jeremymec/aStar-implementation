7a3572fd59544017a29b65acbcd7c795146b687e
jezza97 <jeremymechen1997@gmail.com>
2017-04-16 18:22:18 +1200

    Updated Struct

--- src/AStar.java
+++ src/AStar.java
@@ -1,6 +1,31 @@
-
+import java.util.HashMap;
 
 public class AStar {
 
+    HashMap<Node, Value> nodes;
+    HashMap<Node, Value> visited;
+
+
+}
+
+class Value{
+    // Total cost of Node
+    int cost;
+
+    //heuristic
+    int h;
+
+    int g;
+
+    public Value(int h, int g){
+        this.h = h;
+        this.g = g;
+
+        this.cost = g + h;
+    }
+
+    public int cost(){
+        return this.cost;
+    }
 
 }
